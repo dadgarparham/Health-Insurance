fe0442d6ebe15e9ab501bf452e43be6205e08164
Lenovo <parham.dadgar@outlook.com>
2024-05-06 20:44:55 +0330

    Add CoverageType Enum

--- ClassLibraries/Health_Insurance/Health_Insurance.Application/PremiumCalculateService.cs
+++ ClassLibraries/Health_Insurance/Health_Insurance.Application/PremiumCalculateService.cs
@@ -1,6 +1,8 @@
 using Health_Insurance.Data;
 using Microsoft.EntityFrameworkCore;
 using Health_Insurance.Domain.Entities;
+using Health_Insurance.Domain.Enums;
+
 namespace Health_Insurance.Application;
 
 public class PremiumCalculateService
@@ -23,17 +25,17 @@ public class PremiumCalculateService
 
         decimal totalPremium = 0;
 
-        if (request.Coverage.Name == "جراحی")
+        switch (request.Coverage.CoverageType)
         {
-            totalPremium += request.Amount * 0.0052m;
-        }
-        else if (request.Coverage.Name == "دندانپزشکی")
-        {
-            totalPremium += request.Amount * 0.0042m;
-        }
-        else if (request.Coverage.Name == "بستری")
-        {
-            totalPremium += request.Amount * 0.005m;
+            case (int)CoverageType.SurgicalCoverage: //جراحی
+                totalPremium += request.Amount * 0.0052m;
+                break;
+            case (int)CoverageType.DentalCoverage: //دندانپزشکی
+                totalPremium += request.Amount * 0.0042m;
+                break;
+            case (int)CoverageType.BedCoverage: //بستری
+                totalPremium += request.Amount * 0.005m;
+                break;
         }
 
         request.TotalPremium = totalPremium;
---
+++ ClassLibraries/Health_Insurance/Health_Insurance.Domain/Enums/InsuranceType.cs
@@ -0,0 +1,7 @@
+namespace Health_Insurance.Domain.Enums;
+public enum CoverageType
+{
+    SurgicalCoverage = 1, //جراحى
+    DentalCoverage, //دندانپزشکی
+    BedCoverage, //بسترى
+}
