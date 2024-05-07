c131966ce8bc2d391c8be733454a273d98186c3b
Lenovo <parham.dadgar@outlook.com>
2024-05-08 00:01:14 +0330

    Adding Migration

---
+++ ClassLibraries/Health_Insurance/Health_Insurance.Domain/Entities/Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Health_Insurance.Domain.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static bool IsNullOrEmpty<T>(this IList<T> input)
+    {
+        return input == null || !input.Any();
+    }
+}
---
+++ ClassLibraries/Health_Insurance/Health_Insurance.Domain/Entities/InsuranceRequest/Models/GetInsuranceRequestListDto.cs
@@ -0,0 +1,16 @@
+﻿using Health_Insurance.Domain.Entities.File;
+
+namespace Health_Insurance.Domain.Models;
+public class GetInsuranceRequestListDto
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = null!;
+    public decimal Capital { get; set; }
+    public decimal TotalPremium { get; set; }
+}
+
+
+public class GetInsuranceRequestPagedListModel : PagedListModel<GetInsuranceRequestListDto>
+{
+
+}
