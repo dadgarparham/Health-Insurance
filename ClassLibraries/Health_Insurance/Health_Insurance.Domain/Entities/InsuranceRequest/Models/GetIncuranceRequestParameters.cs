56ae8dd6bbeb44d0002c34fe3110980ef3a13a58
Lenovo <parham.dadgar@outlook.com>
2024-05-08 00:01:14 +0330

    Fix sql connectionstring path

---
+++ ClassLibraries/Health_Insurance/Health_Insurance.Domain/Entities/InsuranceRequest/Models/GetIncuranceRequestParameters.cs
@@ -0,0 +1,9 @@
+﻿using Health_Insurance.Domain.Entities.Base;
+
+namespace Health_Insurance.Domain.Entities;
+public class GetInsuranceRequestParameters:BaseSearchParameters
+{
+    public long? Id { get; set; }
+    public string? Title { get; set; }
+    public decimal? Capital { get; set; }
+}
